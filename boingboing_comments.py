8b173297e48178ffdeb7568c4ddbe8ac2f2bcb90
Cavin Dsouza <cavin46016@gmail.com>
2018-03-13 19:58:39 -0400

    Update boingboing_comments.py

--- boingboing_comments.py
+++ boingboing_comments.py
@@ -1,32 +1,35 @@
 # https://stackoverflow.com/questions/41706274/beautifulsoup-returns-incomplete-html
 """
-The issue is that by default, a qty. of 19 <div class="topic-post clearfix regular"></div> tags on boingboing comments is loaded at 
-page startup and the remaining are loaded once the page is scrolled down. 
-We use selenium web-driver to fix this issue.
+The page setup is such that, by default, approx. 19 <div class="topic-post clearfix regular"></div> tags on boingboing comments are
+loaded at startup and the remaining get loaded once the page is manually scrolled down. 
+We use selenium web-driver to achieve the manual scrolling.
 
-pip install selenium and install geckodriver (https://github.com/mozilla/geckodriver/releases) for firefox 
-(on OSX: brew install geckodriver). 
-Note: geckodriver.exe must be in the same directory as the .py file.
+We need either the geckodriver(https://github.com/mozilla/geckodriver/releases) for firefox 
+or the chromedriver (latest release 2.3.6 --> https://chromedriver.storage.googleapis.com/index.html?path=2.36/)
+
+Note: the executable driver file must be in the same directory as the .py file.
 """
 
-from bs4 import BeautifulSoup
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from datetime import datetime
 import re
 import time
 import math
 
+from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
 SCREEN_HEIGHT_IN_PIXELS = 1080
 COMMENTS_SCREEN_SIZE = 3
 SCROLL_WAIT_TIME = 1
 
-def fetch_comment_info(browser, url, postno, cur, delay = 100):
+def fetch_comment_info(browser, url, postno, cur, delay=100):
     comments = {}
-    # indicates presence of div_class_share but no a_class_bbs
+
     try:
-        # Added timeout for the error: http.client.RemoteDisconnected: Remote end closed connection without response
+        """Added timeout for the error: http.client.RemoteDisconnected: Remote end closed connection without response"""
         browser.set_page_load_timeout(200)
         browser.get(url)
     except:
@@ -47,11 +50,11 @@ def fetch_comment_info(browser, url, postno, cur, delay = 100):
         for li in li_all:
             li_text = li.text.strip()
             span_class_number = li.find("span")
-            str_found = False;
+            str_found = False
             for i in topic_str:
                 if(i in li_text):
-                    str_found = True;
-                    break;
+                    str_found = True
+                    break
                 
             if(str_found and span_class_number):
                 if "k" in span_class_number.text:
@@ -69,13 +72,14 @@ def fetch_comment_info(browser, url, postno, cur, delay = 100):
                         topic_map[i] = num
                     
     """Replies, Views, Users, Likes and Links"""
+    
     tmp = 0            
-    query =  "UPDATE posts SET c_page_url = %s, replies = %s, views = %s, users = %s, likes = %s, links = %s WHERE postno = %s;";
+    query =  "UPDATE posts SET c_page_url = %s, replies = %s, views = %s, users = %s, likes = %s, links = %s WHERE postno = %s;"
     if(topic_map[0] >= 1):
         tmp = topic_map[0] - 1
-    data = (url, tmp, topic_map[1], topic_map[2], topic_map[3], topic_map[4], postno);
+    data = (url, tmp, topic_map[1], topic_map[2], topic_map[3], topic_map[4], postno)
         
-    cur.execute(query, data);
+    cur.execute(query, data)
     """Replies, Views and Likes"""
     
     scrolls = math.ceil(topic_map[0]/COMMENTS_SCREEN_SIZE)
@@ -85,10 +89,10 @@ def fetch_comment_info(browser, url, postno, cur, delay = 100):
         div_class_comment = soup.find_all("div", attrs={"class":"topic-post clearfix regular"}) + \
                                 soup.find_all("div", attrs = {"class":"topic-post clearfix topic-owner group-editors regular"})
                                 
-        comm_no = 1;
+        comm_no = 1
         for c in div_class_comment:
             div_class_user_card = c.find("div", attrs = {"class":"names trigger-user-card"})
-            postedby = None;
+            postedby = None
             if(div_class_user_card):
                 span_class_firstusername = c.find("span")
                 if(span_class_firstusername):
@@ -109,20 +113,20 @@ def fetch_comment_info(browser, url, postno, cur, delay = 100):
                         comments[dict_primary_key]['comm_no'] = comm_no
                         
                         div_class_cooked = c.find("div", attrs={"class":"cooked"})
-                        comments[dict_primary_key]['comm_text'] = comm_text;
+                        comments[dict_primary_key]['comm_text'] = comm_text
                         
                         div_class_actions = c.find("div", attrs={"class":"actions"})
                         comment_like_list = re.findall("\d+", div_class_actions.text.strip())
                         
                         if(comment_like_list):
-                            comment_likes = int(comment_like_list[0]);
+                            comment_likes = int(comment_like_list[0])
                         else:
                             comment_likes = 0
                         comments[dict_primary_key]['likes'] = comment_likes  
                         
-                        comm_no += 1;
+                        comm_no += 1
                         
-        browser.execute_script("window.scrollTo({}, {});".format(i*SCREEN_HEIGHT_IN_PIXELS, (i+1)*SCREEN_HEIGHT_IN_PIXELS))
+        browser.execute_script("window.scrollTo({}, {});".format(i * SCREEN_HEIGHT_IN_PIXELS, (i + 1) * SCREEN_HEIGHT_IN_PIXELS))
         time.sleep(SCROLL_WAIT_TIME)
         
     return comments
