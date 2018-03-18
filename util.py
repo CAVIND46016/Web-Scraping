32cc9f664b2a3848a32f85119d0bc88c052c57f1
Cavin Dsouza <cavin46016@gmail.com>
2018-03-18 03:33:17 -0400

    Update util.py

--- util.py
+++ util.py
@@ -1,13 +1,19 @@
-def connectToDatabaseServer(dbname):
-    import psycopg2
-    # PostGresSQL Params
-    HOST = "localhost"
-    USER = "postgres"
-    PASSWORD = "xxxxxxxxxxx"
-    
+"""
+Utility function to connect to PostgreSQL server.
+"""
+import psycopg2
+
+HOST = "localhost"
+USER = "postgres"
+PASSWORD = "xxxxxxxxxxx"
+def connect_to_database_server(dbname):
+    """
+    Connects to PostgreSQL server database 'dbname' and 
+    returns a connection object and cursor.
+    """
     try:
-        conn = psycopg2.connect(host = HOST, database = dbname, user = USER, password = PASSWORD);
+        conn = psycopg2.connect(host=HOST, database=dbname, user=USER, password=PASSWORD)
         cur = conn.cursor() 
         return [conn, cur]
-    except:
-        return -1; # Connection failure
+    except psycopg2.OperationalError:
+        return -1
